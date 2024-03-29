b8c00fe0ef4445039c36e6183aaa36fdbe96d8d3
Syed Ahmad Shah <117304625+AhmadShah-1@users.noreply.github.com>
2024-03-29 02:21:14 +0000

    Sara mean

---
+++ healthmonitoringapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FitbitDataComponent from './FitbitDataComponent';
+
+function App() {
+  return (
+    <div>
+      <FitbitDataComponent>
+
+      </FitbitDataComponent>
+    </div>
+  );
+}
+
+export default App;
