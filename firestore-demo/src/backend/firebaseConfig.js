13aa7c710ce21cfc62b9e578a760b9d3539af8f5
Sohan Chatterjee <74262451+sohanchatterjee@users.noreply.github.com>
2024-04-22 22:37:25 -0400

    Add files via upload

---
+++ firestore-demo/src/backend/firebaseConfig.js
@@ -0,0 +1,23 @@
+/*
+Get this from Firebase app settings
+*/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJoclJIl0ZmCI9-70h2oWAzaYGdP4u2pg",
+  authDomain: "healthmonitoringapp-152ca.firebaseapp.com",
+  projectId: "healthmonitoringapp-152ca",
+  storageBucket: "healthmonitoringapp-152ca.appspot.com",
+  messagingSenderId: "516085061102",
+  appId: "1:516085061102:web:e030d9c0b2133b4846eaff"
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
