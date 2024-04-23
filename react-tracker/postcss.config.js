145677d10a2e2063eeb33fb30d53fb2a11313268
sohanchatterjee <sohanchatterjee20@gmail.com>
2024-04-22 22:51:24 -0400

    update

--- react-tracker/postcss.config.js
+++
@@ -1,8 +0,0 @@
-import daisyui from "daisyui";
-
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
--- react-tracker/src/fitbit/fitbitConfig.js
+++
@@ -1,5 +0,0 @@
-const ClientID = '23RX8W';
-const ClientSecret = '99c63ffe9999af91fb1e0fcd9ae67802';
-const RedirectUri = 'https://testing-reactjs-integration.web.app/'; // the redirectURL in FitBit app
-
-export {ClientID, ClientSecret, RedirectUri};
